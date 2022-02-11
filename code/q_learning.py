1372ddd6cd92584a757dc9c13f1c7541bfd57a55
Baja-KS <lukic.l210@gmail.com>
2022-02-11 18:42:20 +0100

    Finishing touches added .py and .ipynb variants test results

--- code/q_learning.py
+++ code/q_learning.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[121]:
+# In[175]:
 
 
 import numpy as np
 
 
-# In[122]:
+# In[176]:
 
 
 class Env:
@@ -25,19 +25,19 @@ class Env:
         
         print(self.map)
         
-        self.init_env()
+        self.init_env(self.max_num_obstacles)
         
         self.agent_pos = [0, 0]   
         self.target_pos = [map_height - 2, map_width - 2]
     
     def reset_env(self):
         self.map = np.array(self.map_copy)
-        self.init_env()
+        self.init_env(np.random.randint(0, self.max_num_obstacles + 1))
         self.init_agent_pos()
         self.init_target_pos()
         
-    def init_env(self):
-        for _ in range(np.random.randint(self.max_num_obstacles)):
+    def init_env(self, num_obstacles):
+        for _ in range(num_obstacles):
             obstacle_pos = [0, 0]
             while self.map[obstacle_pos[0], obstacle_pos[1]] == 0:
                 obstacle_pos[0] = np.random.randint(2, self.map_height - 2)
@@ -80,52 +80,74 @@ class Env:
         
         return False
     
-    def step(self, action, prev_action):
+    def step(self, action, prev_action=4):
         reward = 0
         done = False
-        new_view = env.get_view()
         looping = False
         
         if action == 0:
             self.agent_pos[0] -= 1
-            if prev_action == 2:
+            if prev_action == 4:
                 looping = True
                 
         elif action == 1:
+            self.agent_pos[0] -= 1
             self.agent_pos[1] += 1
-            if prev_action == 3:
+            if prev_action == 5:
                 looping = True
                 
         elif action == 2:
             self.agent_pos[0] += 1
-            if prev_action == 0:
+            if prev_action == 6:
                 looping = True
                 
         elif action == 3:
+            self.agent_pos[0] += 1
+            self.agent_pos[1] += 1
+            if prev_action == 7:
+                looping = True
+                
+        elif action == 4:
+            self.agent_pos[0] += 1
+            if prev_action == 0:
+                looping = True
+                
+        elif action == 5:
+            self.agent_pos[0] += 1
             self.agent_pos[1] -= 1
             if prev_action == 1:
+                looping = True
+                
+        elif action == 6:
+            self.agent_pos[1] -= 1
+            if prev_action == 2:
+                looping = True
+                
+        elif action == 7:
+            self.agent_pos[0] -= 1
+            self.agent_pos[1] -= 1
+            if prev_action == 3:
                 looping = True
         
         reward -= 1
         
         if looping:
-            reward - 1000
+            reward -= 2
+        
+        new_view = env.get_view()
         
         if self.map[self.agent_pos[0], self.agent_pos[1]] == 0:
             reward -= 1000
             done = True
-            
-        else:
-            new_view = env.get_view()
         
         if self.target_pos[0] == self.agent_pos[0] and self.target_pos[1] == self.agent_pos[1]:
-            reward += 1000
+            reward += 100
             done = True
         
         return new_view, self.agent_pos, reward, done
 
 
-# In[123]:
+# In[177]:
 
 
 class Agent:
@@ -139,7 +161,7 @@ class Agent:
         self.lr_decay = lr_decay
         self.gamma = gamma
         
-        self.actions = ['u', 'r', 'd', 'l']
+        self.actions = ['u', 'ur', 'r', 'dr', 'd', 'dl', 'l', 'ul']
         
         dims = []
         for i in range(8):
@@ -153,15 +175,23 @@ class Agent:
         
         self.q_values = np.zeros(dims)
         
-        self.q_values[:, 0, :, :, :, :, :, :, :, :, :, :, :, 0] = -10000
-        self.q_values[:, :, :, :, 0, :, :, :, :, :, :, :, :, 1] = -10000
-        self.q_values[:, :, :, :, :, :, 0, :, :, :, :, :, :, 2] = -10000
-        self.q_values[:, :, :, 0, :, :, :, :, :, :, :, :, :, 3] = -10000
+        self.q_values[:, 0, :, :, :, :, :, :, :, :, :, :, :, 0] = -100000
+        self.q_values[:, :, 0, :, :, :, :, :, :, :, :, :, :, 1] = -100000
+        self.q_values[:, :, :, :, 0, :, :, :, :, :, :, :, :, 2] = -100000
+        self.q_values[:, :, :, :, :, :, :, 0, :, :, :, :, :, 3] = -100000
+        self.q_values[:, :, :, :, :, :, 0, :, :, :, :, :, :, 4] = -100000
+        self.q_values[:, :, :, :, :, 0, :, :, :, :, :, :, :, 5] = -100000
+        self.q_values[:, :, :, 0, :, :, :, :, :, :, :, :, :, 6] = -100000
+        self.q_values[0, :, :, :, :, :, :, :, :, :, :, :, :, 7] = -100000
         
-        self.q_values[:, :, :, :, :, :, :, :, :, :, :, :, 0, 2] = -10000
-        self.q_values[:, :, :, :, :, :, :, :, :, :, :, :, 1, 3] = -10000
-        self.q_values[:, :, :, :, :, :, :, :, :, :, :, :, 2, 0] = -10000
-        self.q_values[:, :, :, :, :, :, :, :, :, :, :, :, 3, 1] = -10000
+        self.q_values[:, :, :, :, :, :, :, :, :, :, :, :, 0, 4] = -1000
+        self.q_values[:, :, :, :, :, :, :, :, :, :, :, :, 1, 5] = -1000
+        self.q_values[:, :, :, :, :, :, :, :, :, :, :, :, 2, 6] = -1000
+        self.q_values[:, :, :, :, :, :, :, :, :, :, :, :, 3, 7] = -1000
+        self.q_values[:, :, :, :, :, :, :, :, :, :, :, :, 4, 0] = -1000
+        self.q_values[:, :, :, :, :, :, :, :, :, :, :, :, 5, 1] = -1000
+        self.q_values[:, :, :, :, :, :, :, :, :, :, :, :, 6, 2] = -1000
+        self.q_values[:, :, :, :, :, :, :, :, :, :, :, :, 7, 3] = -1000
         
     def act(self, view, row_index, column_index, target_index_x, target_index_y, prev_action):
         if self.eps > np.random.uniform():
@@ -170,12 +200,20 @@ class Agent:
         
             if view[1] == 1:
                 legal_actions.append(0)
-            if view[3] == 1:
-                legal_actions.append(3)
-            if view[4] == 1:
+            if view[2] == 1:
                 legal_actions.append(1)
-            if view[6] == 1:
+            if view[4] == 1:
                 legal_actions.append(2)
+            if view[7] == 1:
+                legal_actions.append(3)
+            if view[6] == 1:
+                legal_actions.append(4)
+            if view[5] == 1:
+                legal_actions.append(5)
+            if view[3] == 1:
+                legal_actions.append(6)
+            if view[0] == 1:
+                legal_actions.append(7)
         
             n = len(legal_actions)
             if n != 0:
@@ -186,27 +224,33 @@ class Agent:
         return np.argmax(self.q_values[tuple(view + [row_index, column_index, target_index_x, target_index_y, prev_action])])
 
 
-# In[124]:
+# In[178]:
 
 
-env = Env(13, 13, 16)
+max_num_obstacles = 10
 
 
-# In[125]:
+# In[179]:
+
+
+env = Env(13, 13, max_num_obstacles)
+
+
+# In[180]:
 
 
 agent = Agent(env.map_height, env.map_width, 1, 0.99999, 0.1, 0.1, 1, 0.99)
 
 
-# In[126]:
+# In[181]:
 
 
-num_episodes = 500000
-max_steps = 50
-stop_exploring_after = 500000
+num_episodes = 1000000
+max_steps = 40
+stop_exploring_after = num_episodes
 
 
-# In[127]:
+# In[182]:
 
 
 #TRAINING
@@ -263,45 +307,47 @@ for episode in range(num_episodes):
     agent.lr *= agent.lr_decay
 
 
-# In[164]:
+# In[1]:
 
 
-#TESTING
 test_episodes = 10000
-agent.eps = 0.15
+agent.eps = 0.0
 
-tests_passed = 0
+for num_obstacles in range(max_num_obstacles + 1):
 
-env.map = np.array(env.map_copy)
-env.init_env()
-
-print(env.map)
-
-for episode in range(test_episodes):
+    tests_passed = 0
     
-    env.init_agent_pos()
-    env.init_target_pos()
+    print('Map with ' + str(num_obstacles) + ' obstacles. eps: ' + str(agent.eps))
     
-    prev_action = 4
-    target_pos = env.target_pos
-    
-    for step in range(1, max_steps + 1):
-        view = env.get_view()
-        agent_pos = list(env.agent_pos)
-        target_pos = list(env.target_pos)
+    for episode in range(test_episodes):
         
-        
-        action = agent.act(view, agent_pos[0], agent_pos[1],
-                           target_pos[0], target_pos[1], prev_action)
-        
-        view, agent_pos, reward, done = env.step(action, prev_action)
-        
-        prev_action = action
-        
-        if done:
-            if agent_pos[0] == target_pos[0] and agent_pos[1] == target_pos[1]:
-                tests_passed += 1
-            break
+        env.map = np.array(env.map_copy)
+        env.init_env(num_obstacles)
 
-print('Success rate: ' + str(tests_passed * 100 / test_episodes) + '%')
+        env.init_agent_pos()
+        env.init_target_pos()
+
+        prev_action = 4
+        target_pos = env.target_pos
+
+        done = False
+
+        for step in range(1, max_steps + 1):
+            view = env.get_view()
+            agent_pos = list(env.agent_pos)
+            target_pos = list(env.target_pos)
+
+            action = agent.act(view, agent_pos[0], agent_pos[1], target_pos[0], target_pos[1], prev_action)
+
+            view, agent_pos, reward, done = env.step(action, prev_action)
+
+            prev_action = action
+
+            if done:
+                if agent_pos[0] == target_pos[0] and agent_pos[1] == target_pos[1]:
+                    tests_passed += 1
+                break
+
+    print('Success rate: ' + str(tests_passed * 100 / test_episodes) + '%\n')
+    agent.eps += 0.011
 
